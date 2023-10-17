75426eaad298126ae18324d1c12349932f8e94e9
Suoireba <suoirebasylla@gmail.com>
2023-10-17 07:16:23 -0700

    number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int digit;
+
+
+	while (digit =0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
