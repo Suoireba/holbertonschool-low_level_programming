2e7eeee0fc8b35ed804e124025bd960723176b71
Suoireba <suoirebasylla@gmail.com>
2023-10-17 07:05:46 -0700

    number

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,10 +3,10 @@ nclude <stdio.h>
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
