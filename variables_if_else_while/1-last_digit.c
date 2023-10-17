d04f0dde19e3b97b65ca953399edfcf4e6df8c29
Suoireba <suoirebasylla@gmail.com>
2023-10-17 03:13:40 -0700

    lasst digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("%d and is 0\n", n % 10);
 	else
-		printf("%d and is 6\n", n % 10);
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	return (0);
 }
