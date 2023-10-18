a461bd8e1d611e76aa2339f6136d57a4ac13098a
Suoireba <suoirebasylla@gmail.com>
2023-10-18 06:06:50 -0700

    printalpha

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -  program that prints _putchar, followed by a new line
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
