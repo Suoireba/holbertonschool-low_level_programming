774e4402e6003e6b3f1e687b07a2a47955f48ccc
Suoireba <suoirebasylla@gmail.com>
2023-10-19 05:20:43 -0700

    ddj

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,12 +6,15 @@
  * @c: The character to be checked.
  *Return: 1 if character is lowercase , 0 otherwise.
  */
-int _islower(int c);
-
+int _islower(int c)
 {
 	
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
