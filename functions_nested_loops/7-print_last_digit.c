a32d2309810ad78c777a41ae84f7765acb96c07b
Suoireba <suoirebasylla@gmail.com>
2023-10-20 04:52:15 -0700

    print

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * print_last_digit - Check for a integer
+ * @r: The character to be checked
+ * Return: Returns the value of the last digit
+ */
+int print_last_digit(int);
+{
+	        return print_last_digit(r);
+}
