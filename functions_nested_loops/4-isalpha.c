def049a9628b2cd20e6a1f1d54aa4a7ae855d5f5
Suoireba <suoirebasylla@gmail.com>
2023-10-20 00:26:22 -0700

    isalpha*

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isalpha - Checks for lowercase character
+ * @c: The character to be checked.
+ * Return: 1 if character is lowercase , 0 otherwise.
+ */
+int isalpha(int c)
+{
+
+	int e;
+	if (isalpha(c))
+	{
+		e = 1;
+	}
+	else
+	{
+		e = 0;
+	
+	}
+	return (e);
+
+}
