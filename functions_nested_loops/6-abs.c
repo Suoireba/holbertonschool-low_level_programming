fc5ddc680b4f85af9e0ea97b9e6a67307fac3b60
Suoireba <suoirebasylla@gmail.com>
2023-10-20 02:19:45 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * abs - Check for a integer
+ * @98: The character to be checked
+ *  Return: the absolute value of an integer.
+ */
+int _abs(int);
+{
+	abs(98);
+
+}
