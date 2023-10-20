bb9c2f85d9806c150c7db7fa8a537e18c96a4b00
Suoireba <suoirebasylla@gmail.com>
2023-10-20 01:01:28 -0700

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_sign: prints the sign of a number.
+ * @c: The character t be checked
+ * Return: For return of sign of a number , 0 otherwise.
+ */
+int print_sign(int n);
+{
+	int temp;
+
+	if (print_sign(n))
+	{
+		temp = 1;
+	}
+	else
+	{
+		temp = 0;
+	}
+	return (temp);
+
+}
