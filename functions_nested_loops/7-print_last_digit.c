6fc71b7bea65df156f6d404e65143b42ea4affaf
Suoireba <suoirebasylla@gmail.com>
2023-10-20 05:06:53 -0700

    prit last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include 'main.h'
+#include "main.h"
 /**
  * print_last_digit - Check for a integer
  * @r: The character to be checked
@@ -9,7 +9,7 @@
  */
 int print_last_digit(int)
 {
-	int (r %10);
+	int r % 10;
 	_putchar ('0' + 'r');
 	return (r);	
 }
